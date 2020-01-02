f7fa4c91be256b11e02c3079cebd0671c673899b
reagul <reagul.2007@gmail.com>
2020-01-02 15:35:56 -0500

    V1

--- main.go
+++ main.go
@@ -1,147 +1,75 @@
-// +build go1.7
-
 package main
 
 import (
-    "context"
-    "fmt"
-    "time"
+	"fmt"
 
-    "github.com/gomodule/redigo/redis"
+	"github.com/go-redis/redis"
 )
 
-// listenPubSubChannels listens for messages on Redis pubsub channels. The
-// onStart function is called after the channels are subscribed. The onMessage
-// function is called for each message.
-func listenPubSubChannels(ctx context.Context, redisServerAddr string,
-    onStart func() error,
-    onMessage func(channel string, data []byte) error,
-    channels ...string) error {
-    // A ping is set to the server with this period to test for the health of
-    // the connection and server.
-    const healthCheckPeriod = time.Minute
+//
 
-    c, err := redis.Dial("tcp", redisServerAddr,
-        // Read timeout on server should be greater than ping period.
-        redis.DialReadTimeout(healthCheckPeriod+10*time.Second),
-        redis.DialWriteTimeout(10*time.Second))
-    if err != nil {
-        return err
-    }
-    defer c.Close()
-
-    psc := redis.PubSubConn{Conn: c}
-
-    if err := psc.Subscribe(redis.Args{}.AddFlat(channels)...); err != nil {
-        return err
-    }
-
-    done := make(chan error, 1)
-
-    // Start a goroutine to receive notifications from the server.
-    go func() {
-        for {
-            switch n := psc.Receive().(type) {
-            case error:
-                done <- n
-                return
-            case redis.Message:
-                if err := onMessage(n.Channel, n.Data); err != nil {
-                    done <- err
-                    return
-                }
-            case redis.Subscription:
-                switch n.Count {
-                case len(channels):
-                    // Notify application when all channels are subscribed.
-                    if err := onStart(); err != nil {
-                        done <- err
-                        return
-                    }
-                case 0:
-                    // Return from the goroutine when all channels are unsubscribed.
-                    done <- nil
-                    return
-                }
-            }
-        }
-    }()
-
-    ticker := time.NewTicker(healthCheckPeriod)
-    defer ticker.Stop()
-loop:
-    for err == nil {
-        select {
-        case <-ticker.C:
-            // Send ping to test health of connection and server. If
-            // corresponding pong is not received, then receive on the
-            // connection will timeout and the receive goroutine will exit.
-            if err = psc.Ping(""); err != nil {
-                break loop
-            }
-        case <-ctx.Done():
-            break loop
-        case err := <-done:
-            // Return error from the receive goroutine.
-            return err
-        }
-    }
-
-    // Signal the receiving goroutine to exit by unsubscribing from all channels.
-    psc.Unsubscribe()
-
-    // Wait for goroutine to complete.
-    return <-done
-}
-
-func publish() {
-    c, err := dial()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer c.Close()
-
-    c.Do("PUBLISH", "c1", "hello")
-    c.Do("PUBLISH", "c2", "world")
-    c.Do("PUBLISH", "c1", "goodbye")
-}
-
-// This example shows how receive pubsub notifications with cancelation and
-// health checks.
 func main() {
-    redisServerAddr, err := serverAddr()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	/* // main function
 
-    ctx, cancel := context.WithCancel(context.Background())
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
 
-    err = listenPubSubChannels(ctx,
-        redisServerAddr,
-        func() error {
-            // The start callback is a good place to backfill missed
-            // notifications. For the purpose of this example, a goroutine is
-            // started to send notifications.
-            go publish()
-            return nil
-        },
-        func(channel string, message []byte) error {
-            fmt.Printf("channel: %s, message: %s\n", channel, message)
+	pong, err := rdb.Ping().Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	print(pong)
 
-            // For the purpose of this example, cancel the listener's context
-            // after receiving last message sent by publish().
-            if string(message) == "goodbye" {
-                cancel()
-            }
-            return nil
-        },
-        "c1", "c2")
+	pubsub := rdb.Subscribe("PODLIST")
 
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	// Wait for confirmation that subscription is created before publishing anything.
+	for i := 0; i < 2; i++ {
+		// ReceiveTimeout is a low level API. Use ReceiveMessage instead.
+		msgi, err := pubsub.ReceiveTimeout(time.Second)
+		if err != nil {
+			break
+		}
+
+		switch msg := msgi.(type) {
+		case *redis.Subscription:
+			fmt.Println("subscribed to", msg.Channel)
+
+			/*
+				_, err := rdb.Publish("PODLIST", "hello").Result()
+				if err != nil {
+					panic(err)
+				}
+		case *redis.Message:
+			fmt.Println("received", msg.Payload, "from", msg.Channel)
+		default:
+			panic("unreached")
+		}
+
+	} */
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	podlistchannel := client.Subscribe("PODLIST")
+	somemsg, err := podlistchannel.ReceiveMessage()
+	if err != nil {
+		panic(err)
+	}
+
+	//for {
+	fmt.Println(somemsg.Payload)
+	if somemsg.Payload == "blog" {
+		fmt.Println("kickoff new workglow")
+	}
+	//time.Sleep(time.Second)
+	//}
 
 }
